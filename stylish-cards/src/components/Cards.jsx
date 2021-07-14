a4a41e53422b25dbac505d17847952800d5ee830
CarstenHennig <carsten.hennig@gmail.com>
2021-07-14 13:33:41 +0200

    More styled

--- stylish-cards/src/components/Cards.jsx
+++ stylish-cards/src/components/Cards.jsx
@@ -2,20 +2,43 @@ import React from 'react'
 import styled from 'styled-components';
 
     const Button = styled.button`
-    color: red;
-    border: 1px solid green;
+        color: red;
+        border: 1px solid green;
     `;
 
+    const Heading = styled.h3`
+        color: tomato;    
+    `
+
+    const SubHeader = styled.h5`
+        color: darkred;
+    `
+
+    const Text = styled.p`
+        color: darkgreen;
+        font-family: monospace;
+        font-style: normal;
+        font-weight: bold;
+        font-display: inline;
+    `
+
+    const Links = styled.a`
+        color: gray;
+        font-style: underline;
+        font-size: small;
+    `
+
     const CardOne = styled.div`
-        color: green;
+        background-color: rgba(46, 243, 9, 0.2); 
+        color: rgba(46, 243, 9, 0.2);
         border: 1px solid darkblue;
-        width: "80%";
-        height: "20%";
+        width: 80;
+        height: 20;
     `
 
     const CardTwo = styled.div`
         color: pale;
-        background-color: gray;
+        background-color: lightgray;
         border: 1px solid orange;
         width: "60%":
         height: "15%";
@@ -23,7 +46,7 @@ import styled from 'styled-components';
 
     const CardThree = styled.div`
         color: blue;
-        background-color: cyan;
+        background-color: lightcyan;
         border: 1px solid black;
         width: 50;
         height: 20;
@@ -42,18 +65,30 @@ import styled from 'styled-components';
     return (
         <div>
             <CardOne>
-             <Button>Button</Button>
+                <Heading>This is a Heading</Heading>
+                <SubHeader>This is a Subheading</SubHeader>
+                <Text>Lorem ipsum dolor sit amet, consectetur</Text>
+                <Links><a href="www.digitalcareerinstitute.org">Digital Career Institute</a></Links>
             </CardOne>    
 
             <CardTwo>
+                <Heading>This is a Heading</Heading>
+                <SubHeader>This is a Subheading</SubHeader>
+                <Text>Lorem ipsum dolor sit amet, consectetur</Text>
                 <Button>Button</Button>
             </CardTwo> 
 
             <CardThree>
+                <Heading>This is a Heading</Heading>
+                <SubHeader>This is a Subheading</SubHeader>
+                <Text>Lorem ipsum dolor sit amet, consectetur</Text>
                 <Button>Button</Button>
             </CardThree>
 
             <CardFour>
+                 <Heading>This is a Heading</Heading>
+                 <SubHeader>This is a Subheading</SubHeader>
+                 <Text>Lorem ipsum dolor sit amet, consectetur</Text>
                 <Button>Button</Button>
             </CardFour> 
 
